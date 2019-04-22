4f748608852ba16a09400f9bdb3f2fc30448b13b
Doritanh <charrier.antho99@gmail.com>
2019-04-22 22:34:20 +0200

    Reponse a l'envoyeur

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 /*
 **  Cobalt Core project
 */
-'use strict';
+'use strict'
 
-const net = require('net');
+const net = require('net')
 
 const server = net.createServer((socket) => {
-    socket.end('goodbye !');
+  let clients = new Clients()
+
+  socket.on('data', (data) => {
+    data = JSON.parse(data.toString(this.utf8))
+    if (data.action === 'auth' && data.sender !== null) {
+      clients.add(data.sender, socket)
+    } else if (data.action === 'message') {
+      clients.get(data.to).write('Reponse')
+    }
+  })
 })
 
-server.listen(4541);
+server.on('connection', (connection) => {
+  console.log(connection)
+})
+
+server.listen(4541)
+
+class Clients {
+  constructor () {
+    this.clients = {}
+  }
+}
+
+Clients.prototype.add = function (sender, socket) {
+  this.clients[sender] = socket
+}
+
+Clients.prototype.get = function (addr) {
+  return this.clients[addr]
+}
