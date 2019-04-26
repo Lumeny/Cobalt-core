99162cd83e2487a329bb855746507373d12afa15
Doritanh <charrier.antho99@gmail.com>
2019-04-26 11:04:37 +0200

    Cobalt events

--- index.js
+++ index.js
@@ -5,18 +5,33 @@
 const net = require('net');
 const Cobalt = require('./src/cobalt');
 
+const PORT = 4541
+
+console.log('[COBALT-CORE] Starting server...');
+
 const server = net.createServer((socket) => {
-    let cobalt = new Cobalt(socket);
-    socket.on('data', (data) => {cobalt.parse(data)});
+    let clients = new Clients();
+    let cobalt = new Cobalt();
+    cobalt.on('auth', (response) => {
+        clients.add(response.to, socket);
+        clients.get(response.to).write(JSON.stringify(response));
+    });
+    cobalt.on('response', (response) => {
+        clients.get(response.to).write(JSON.stringify(response))
+    });
+    socket.on('data', (data) => {
+        cobalt.parse(data)
+    });
 });
 
-server.listen(4541);
+server.listen(PORT);
+
+console.log('[COBALT-CORE] Running on port : ' + PORT);
 
 
-/*
 class Clients {
     constructor () {
-    this.clients = {}
+        this.clients = {}
     }
 }
 
@@ -27,4 +42,3 @@ Clients.prototype.add = function (sender, socket) {
 Clients.prototype.get = function (addr) {
     return this.clients[addr]
 }
-*/
