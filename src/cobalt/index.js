c41ebcc55225113dd9303439ee341208a6f501d5
Doritanh <charrier.antho99@gmail.com>
2019-04-26 11:04:49 +0200

    New parser

--- src/cobalt/index.js
+++ src/cobalt/index.js
@@ -1,26 +1,37 @@
 'use strict';
+const EventEmitter = require('events');
 
-class Cobalt {
-    constructor(socket) {
-        this._from = socket;
+class Cobalt extends EventEmitter {}
+
+Cobalt.prototype.parse = function(data) {
+    /*
+    **  If 2 messages are send in the same time,
+    **  there will be a bug and the string will be like
+    **  {...}{...} so we need to separe them.
+    */
+    data = data.toString().split('}');
+    for (let i = 0; i < data.length; i++) {
+        if (data[i] !== '') this._parse(JSON.parse(data[i] + '}'));
     }
 }
 
-Cobalt.prototype.parse = function(data) {
-    data = JSON.parse(data.toString());
+Cobalt.prototype._parse = function(data) {
+    console.log(data)
     switch (data.action) {
         case 'auth':
-            /*if (data.sender !== null) {
-            clients.add(data.sender, socket)
-            }*/
+            this.emit('auth', {'from':'server', 'to': data.from, 'action':'auth', 'content':'ok'});
             break;
         case 'ping':
-            this._from.write(JSON.stringify({'from':'server', 'action':'ping', 'content':'pong'}));
+            this.emit('response', {'from':'server', 'to': data.from, 'action':'ping', 'content':'pong'});
             break;
         case 'message':
             //clients.get(data.to).write('Reponse')
             break;
     }
+}
+
+Cobalt.prototype.checkIntegrity = function(data) {
+
 }
 
 module.exports = Cobalt;
