935e7c8378b1c7639417aaf2d49e35af80e2d024
Doritanh <charrier.antho99@gmail.com>
2019-04-26 11:04:59 +0200

    New tests for auth

--- test/test.js
+++ test/test.js
@@ -2,21 +2,44 @@ const assert = require('assert');
 const net = require('net');
 
 const protocol = {
-    'sendPing' : {
-        'action' : 'ping'
+    'sendServerPing' : {
+        'action' : 'ping',
+        'to' : 'server',
+        'from' : 'test'
+    },
+    'authentificate' : {
+        'action':'auth',
+        'from':'test',
+        'to':'server'
     }
 }
 
 describe('Connexion', function() {
-    it('Should return pong', function(done) {
-        const client = net.createConnection({port:4541}, () => {
-            client.write(JSON.stringify(protocol.sendPing));
+    it('Should authentificate', function(done) {
+        let client = net.createConnection({port:4541}, () => {
+            client.write(JSON.stringify(protocol.authentificate));
         });
         client.on('data', (data) =>  {
             client.end();
-            if (JSON.parse(data.toString()).content !== 'pong') done(false);
+            if (JSON.parse(data.toString()).content !== 'ok') done(false);
             done();
-            
+        });
+    });
+
+    it('Should return pong', function(done) {
+        let client = net.createConnection({port:4541}, () => {
+            client.write(JSON.stringify(protocol.authentificate));
+            client.write(JSON.stringify(protocol.sendServerPing));
+        });
+        client.on('data', (data) =>  {
+            data = JSON.parse(data.toString());
+            if (data.action === 'auth') return;
+            if (data.content === 'pong') {
+                client.end();
+                done();
+            } else {
+                done(false);
+            }
         });
     });
 });
