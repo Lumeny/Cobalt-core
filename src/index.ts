c9503ff1b8a972c331e8c59b392b065e22e12c3a
Doritanh <charrier.antho99@gmail.com>
2019-09-01 14:17:57 +0200

    Better logger

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import logger from './logger/index';
 import { API } from './api/index';
 
 let api = new API();
 
 api.listen(3000);
+
+logger.log('Logger fonctionne !')
--- src/logger/index.ts
+++ src/logger/index.ts
@@ -6,4 +6,24 @@ class Console {
     constructor() {
         this.level = 0;
     }
+
+    public log(str: string): void {
+        console.log(str)
+    }
+
+    public info(str: string): void {
+        console.info(str);
+    }
+
+    public warn(str: string): void {
+        console.warn(str);
+    }
+
+    public error(str: string): void {
+        console.error(str);
+    }
 }
+
+let logger: Console = new Console();
+
+export default logger;
