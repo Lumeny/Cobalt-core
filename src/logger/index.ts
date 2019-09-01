63ed3025d50c02d0fce3fc95ba3b429238895c4d
Doritanh <charrier.antho99@gmail.com>
2019-09-01 14:19:25 +0200

    Remove old logger

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -27,3 +27,59 @@ class Console {
 let logger: Console = new Console();
 
 export default logger;
+
+/*
+Old logger file. Must be converted into this one.
+
+'use strict';
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
+
+const consoleFormat = printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const loggerProd = createLogger({
+    level: 'info',
+    format: combine(
+        format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        format.errors({ stack: true }),
+        format.splat(),
+        format.json()
+    ),
+    transports: [
+        new transports.File({ filename: 'logs/errors.log', level: 'error' }),
+        new transports.File({ filename: 'logs/all.log' })
+    ]
+});
+
+const loggerDev = createLogger({
+    level: 'debug',
+    format: format.combine(
+        format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        format.errors({ stack: true }),
+        format.splat(),
+        format.json()
+    ),
+    transports: [
+        new transports.File({ filename: 'logs/errors.log', level: 'error' }),
+        new transports.File({ filename: 'logs/all.log' }),
+        new transports.Console({
+            format: format.combine(
+                label({ label: (process.env.COBALT_NAME || 'COBALT-CORE') }),
+                timestamp(),
+                format.colorize(),
+                consoleFormat
+            )
+        })
+    ]
+});
+
+module.exports = function() {
+    return (process.env.NODE_ENV === 'production') ? loggerProd : loggerDev;
+};
+*/
