89d3704c909fa4fb00b6e27cc1f3840b9e62285a
Doritanh <charrier.antho99@gmail.com>
2019-09-01 14:07:14 +0200

    Remove dist

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var index_1 = require("./api/index");
-var api = new index_1.API();
-api.listen(3000);
--- dist/socket/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-console.log('Not implemented yet');
